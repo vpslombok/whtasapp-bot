a4f0a1a4e707559eb9fca181f2e893db5ae81b7f
vpslombok <136515134+vpslombok@users.noreply.github.com>
2024-08-29 09:37:39 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,33 +1,42 @@
 const mysql = require('mysql2');
 
-// Buat pool koneksi
-const pool = mysql.createPool({
-  host: 'bdeaoblcrmhzuzixz9lu-mysql.services.clever-cloud.com', // Ganti dengan IP server MySQL Anda
-  user: 'uaihieeeuy2yjzsi', // Ganti dengan username MySQL Anda
-  password: 'aVw6FpQ2JcmOlbvLGWgt', // Ganti dengan password MySQL Anda
-  database: 'bdeaoblcrmhzuzixz9lu', // Nama database Anda
-  waitForConnections: true, // Tunggu jika semua koneksi sibuk
-  connectionLimit: 10, // Batas maksimum koneksi
-  queueLimit: 0 // Tidak ada batasan antrian
-});
+let connection;
 
-// Fungsi untuk mengeksekusi query dengan pool
-function executeQuery(query, params) {
-  return new Promise((resolve, reject) => {
-    pool.query(query, params, (error, results) => {
-      if (error) {
-        console.error('Error executing query:', error.message);
-        reject(error);
-      } else {
-        resolve(results);
-      }
-    });
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: 'bdeaoblcrmhzuzixz9lu-mysql.services.clever-cloud.com', // IP atau Hostname MySQL
+    user: 'uaihieeeuy2yjzsi', // Username MySQL Anda
+    password: 'aVw6FpQ2JcmOlbvLGWgt', // Password MySQL Anda
+    database: 'bdeaoblcrmhzuzixz9lu',
+    connectTimeout: 10000, // Timeout koneksi (dalam milidetik)
+    reconnect: true // Tambahkan opsi reconnect
+  });
+
+  // Sambungkan ke database
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err.message);
+      setTimeout(handleDisconnect, 2000); // Coba ulangi setelah 2 detik
+    } else {
+      console.log('Connected to MySQL database.');
+    }
+  });
+
+  // Tangani error yang terjadi selama koneksi
+  connection.on('error', (err) => {
+    console.error('MySQL error:', err.message);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.log('Koneksi ke MySQL terputus, mencoba menyambungkan ulang...');
+      handleDisconnect(); // Sambungkan ulang
+    } else if (err.fatal) {
+      console.log('Kesalahan fatal, mencoba menyambungkan ulang...');
+      handleDisconnect(); // Sambungkan ulang
+    } else {
+      throw err; // Error lain, lemparkan
+    }
   });
 }
 
+handleDisconnect();
 
-
-connectToWhatsApp();
-
-// Ekspor pool jika Anda ingin menggunakannya di modul lain
-module.exports = pool;
+module.exports = connection;
